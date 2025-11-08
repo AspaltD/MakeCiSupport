8f46298f0ef6dcaa3f18a521a40178d9431aa15e
AspaltD <asufaru1635@gmail.com>
2025-11-08 10:27:37 +0900

    タブコンテナの階層がごちゃって来た。要改善

--- frmMakeCi.py
+++ frmMakeCi.py
@@ -11,19 +11,19 @@ class NaviButton(ft.FilledButton):
         self.text = self.select_text(workIdx)
 
     def select_text(self, idx:int):
-        self.b_text: str
+        b_text: str
         match idx:
             case 0:
-                self.b_text = "動作設定"
+                b_text = "動作設定"
             case 1:
-                self.b_text = "読取結果"
+                b_text = "読取結果"
             case 2:
-                self.b_text = "Builderログ"
+                b_text = "Builderログ"
             case 3:
-                self.b_text = "Builder動作完了"
+                b_text = "Builder動作完了"
             case _:
-                self.b_text = "Null Button"
-        return self.b_text
+                b_text = "Null Button"
+        return b_text
 
 class NaviDownMark(ft.Text):
     def __init__(self):
@@ -44,19 +44,74 @@ class NaviBar(ft.Column):
         #self.tight = False
         #self.alignment = ft.MainAxisAlignment.CENTER
         #self.expand = True
-    
+
+class TabBottomButton(ft.Button):
+    def __init__(self, pageIdx:int, workIdx:int=0, buttonClicked):
+        super().__init__()
+        self.pageIdx = pageIdx
+        self.workIdx = workIdx
+        self.on_click = buttonClicked
+        self.text = self.select_text()
+        self.width = 40
+
+    #? workIdx = 0:Exit, 1:Next
+
+    def select_text(self):
+        re_text:str
+        if self.workIdx == 0:
+            re_text = "ExitApp"
+        elif self.workIdx == 1:
+            re_text = "Next"
+        else:
+            re_text = "--Null--"
+        match self.pageIdx:
+            case 0:
+                if self.workIdx == 1:
+                    re_text = "Read"
+            case 1:
+                if self.workIdx == 1:
+                    re_text = "Save&Ins"
+            case 2:
+                if self.workIdx == 1:
+                    re_text = "Stop"
+            case 3:
+                if self.workIdx == 1:
+                    #! MakeMiを実装したら "MakeMi" に変更
+                    re_text = "Next"
+            case 4:
+                if self.workIdx == 1:
+                    re_text = "Stop"
+            case 5:
+                if self.workIdx == 1:
+                    re_text = "Stop"
+            case _:
+                pass
+        return re_text
+
 class TabContents(ft.Container):
     def __init__(self, workIdx):
         super().__init__()
         self.workIdx = workIdx
-        self.expand = 3
-        self.height = 540
+        self.expand = True
+        #self.height = 540
         self.bgcolor = ft.Colors.LIGHT_BLUE_100
         #self.border = ft.border.all(1,ft.Colors.BLACK)
+
+        self.bottomButtons = ft.Row(
+            expand=1,
+            alignment=ft.MainAxisAlignment.END,
+            controls=[
+                #TabBottomButton(pageIdx=0)
+            ]
+        )
+
         self.content = ft.Column(
+            expand=True,
             controls=[
                 ft.Text(value="TabContents_0")
-            ]
+            ],
+            #expand_loose=False
+
         )
 
 
@@ -75,6 +130,17 @@ class MakeCiApp(ft.Container):
             content = self.naviBar
         )
         self.tabContents = TabContents(0)
+        self.tabContentsBase = ft.Container(
+            expand = 3,
+            bgcolor = ft.Colors.DEEP_ORANGE_100,
+            content = ft.Stack(
+                expand=True,
+                controls=[
+                    self.tabContents
+                ]
+            )
+            
+        )
 
         #? パーツの個別設定
         self.naviBar.controls = [
@@ -91,7 +157,7 @@ class MakeCiApp(ft.Container):
         self.content = ft.Row(
             controls=[
                 self.naviBarC,
-                self.tabContents
+                self.tabContentsBase
             ]
         )
 
@@ -99,6 +165,12 @@ class MakeCiApp(ft.Container):
     def button_clicked(self, e):
         workIdx: int = e.control.workIdx
         print(f"{workIdx}_Button clicked")
+        print(self.tabContents.visible)
+        if self.tabContents.visible:
+            self.tabContents.visible = False
+        else:
+            self.tabContents.visible = True
+        
 
         self.update()
 
