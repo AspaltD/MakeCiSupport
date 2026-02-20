7fb4e9be24f3c51b006926ea5b7bccc0ae218105
AspaltD <asufaru1635@gmail.com>
2026-02-20 13:35:25 +0900

    容器としての再構築完了。今ベータから右側部分を一連のパーツとして再定義。

---
+++ app/beta6.0/frmApp.py
@@ -0,0 +1,41 @@
+import flet as ft
+import logging
+
+import enEnums as en
+import frmInterfaces as itf
+
+
+class AppMainFrame(ft.Container):
+    def __init__(self):
+        super().__init__(
+            expand=True,
+        )
+        self.leftTabChBar = itf.Left_box_TabChangeBar()
+        self.tab99 = itf.Rgt_tab_99_PlaceHolder()
+        self.rgtTabs = ft.Stack(
+            expand=3,
+            controls=[
+                self.tab99,
+            ]
+        )
+        self.content = ft.Row(
+            expand=True,
+            controls=[
+                self.leftTabChBar,
+                self.rgtTabs,
+            ]
+        )
+
+def main(page: ft.Page):
+    page.title = "Make CI Support App"
+    page.window.width = 800
+    page.window.height = 605
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    appMainFrame = AppMainFrame()
+    page.add(appMainFrame)
+    page.window.center()
+    page.update()
+
+if __name__ == '__main__':
+    ft.app(target=main)
--- app/beta6.0/frmInterfaces.py
+++ app/beta6.0/frmInterfaces.py
@@ -1,11 +1,11 @@
 import flet as ft
-from typing import List
+from typing import List,Dict
 
 import enEnums as en
 
 
 #*タブ変更欄でボタンの間に置く用のテキスト
-class Left_txt_DownMark(ft.Text):
+class _Left_txt_DownMark(ft.Text):
     def __init__(self, value:str="▼"):
         super().__init__(
             width=180,
@@ -13,8 +13,8 @@ class Left_txt_DownMark(ft.Text):
             value=value
         )
 
-#*タブ変更欄のタブとリンクするボタン
-class Left_btn_TabChange(ft.FilledButton):
+#?タブ変更欄のタブとリンクするボタン
+class _Left_btn_TabChange(ft.FilledButton):
     def __init__(self, tab_idx:en.TabIdx):
         super().__init__(
             width=150,
@@ -44,12 +44,13 @@ class Left_box_TabChangeBar(ft.Container):
         controls:List[ft.Control] = []
         for _tabIdx in en.TabIdx:
             if _tabIdx.name == 'PLACE_HOLDER': continue
-            controls.append(Left_btn_TabChange(_tabIdx))
-            controls.append(Left_txt_DownMark())
+            controls.append(_Left_btn_TabChange(_tabIdx))
+            controls.append(_Left_txt_DownMark())
         controls.pop()
         return controls
 
-#*ボトムボタンの属性のうちタブに依存するものを抜き出したもの
+#?ボトムボタンの属性のうちタブに依存するものを抜き出したもの
+#!ボトムボタンをタブコンテナの配下にするため使わない可能性大
 class Btm_data_BtnProps():
     def __init__(self,
                 btm_btn_idx:en.BtmBtnIdx,
@@ -69,10 +70,11 @@ class Btm_data_BtnProps():
         self.ON_CLICK = on_click
 
 #*タブの下部に置く固有の機能を持たせるためのボタン。
-class Btm_btn_TabFunc(ft.FilledButton):
+class _Btm_btn_TabFunc(ft.FilledButton):
     def __init__(self, btm_btn_idx:en.BtmBtnIdx):
         super().__init__(
-            width=120
+            width=120,
+            disabled=True,
         )
         self.btmBtnIdx = btm_btn_idx
         self.text = self.btmBtnIdx.get_def_txt()
@@ -84,3 +86,60 @@ class Btm_btn_TabFunc(ft.FilledButton):
         self.disabled = props.DISABLED
         self.text = props.TEXT
         self.on_click = props.ON_CLICK
+
+#?タブ固有ボタンたち。すべて変数として生成してるのでボタンに追加があったら任意で追加する。
+class Btm_row_FuncBtns(ft.Row):
+    def __init__(self):
+        super().__init__(
+            expand=1,
+            alignment=ft.MainAxisAlignment.END,
+        )
+        self.btnFunc2 = _Btm_btn_TabFunc(en.BtmBtnIdx.OTHER_FUNC2)
+        self.btnFunc1 = _Btm_btn_TabFunc(en.BtmBtnIdx.OTHER_FUNC1)
+        self.btnExit = _Btm_btn_TabFunc(en.BtmBtnIdx.EXIT_APP)
+        self.btnNext = _Btm_btn_TabFunc(en.BtmBtnIdx.NEXT_TAB)
+        self.controls = [
+            self.btnFunc2,
+            self.btnFunc1,
+            self.btnExit,
+            self.btnNext
+        ]
+
+#*タブのコンテンツたちを配置するためのコンテナ。
+#*以前の構造と違ってこいつには容器としての定義しか持たせてない。
+class _Rgt_box_TabContainer(ft.Container):
+    def __init__(self):
+        super().__init__(
+            expand=10,
+            padding=10,
+            bgcolor=ft.Colors.GREY_50,
+            border=ft.border.all(1, ft.Colors.random())
+        )
+        self.tabItems:List[ft.Control] = []
+
+#?Beta6.0から新たにタブコンテンツと下部ボタンたちを包括した右側パーツとして定義。
+#?タブごとに下部ボタンを持たせることで，オブジェクトの増加と引き換えに構造の簡略化を図る目的。
+#?タブの固有機能はここに持たせる。
+class _Rgt_col_TabBase(ft.Column):
+    def __init__(self, tab_idx:en.TabIdx, dflt_visible:bool=False):
+        super().__init__(
+            expand=True,
+            spacing=2,
+            visible=dflt_visible
+        )
+        self.tabIdx = tab_idx
+        self.tabCont = _Rgt_box_TabContainer()
+        self.btmBtns = Btm_row_FuncBtns()
+        self.controls = [
+            self.tabCont,
+            self.btmBtns
+        ]
+
+#*配置スペースを維持するためのダミータブ。常に最背面に表示。
+class Rgt_tab_99_PlaceHolder(_Rgt_col_TabBase):
+    def __init__(self):
+        super().__init__(tab_idx=en.TabIdx.PLACE_HOLDER, dflt_visible=True)
+        self.tabCont.content = ft.Placeholder(color=ft.Colors.random())
+
+
+
