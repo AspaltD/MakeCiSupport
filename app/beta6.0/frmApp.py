51a5b6c538c2eb6a261874240ed735f07296a077
AspaltD <asufaru1635@gmail.com>
2026-02-25 16:05:19 +0900

    セルデータ用のリストクラス作成中。前回のメインフレームの移行を差し戻し。

--- app/beta6.0/frmApp.py
+++ app/beta6.0/frmApp.py
@@ -5,9 +5,57 @@ import enEnums as en
 import frmInterfaces as itf
 
 
-class AppMainFrame(itf.AppMainFrame):
+
+
+
+class AppMainFrame(ft.Container):
     def __init__(self):
-        super().__init__()
+        super().__init__(
+            expand=True,
+        )
+        self.leftTabChBar = itf.Left_box_TabChangeBar()
+        self.tab99 = itf.Rgt_tab_99_PlaceHolder()
+        self.tab0 = itf.Rgt_tab_0_CIFSelect()
+        self.tab1 = itf.Rgt_tab_1_CIFPreview()
+        self.tab2 = itf.Rgt_tab_2_AppLogs()
+        self.tab3 = itf.Rgt_tab_3_BuilderResult()
+        self.tab4 = itf.Rgt_tab_4_MISelect()
+        self.tab5 = itf.Rgt_tab_5_GJFPreview()
+        self.rgtTabs = ft.Stack(
+            expand=3,
+            controls=[
+                self.tab99,
+                self.tab2,
+                self.tab0,
+                self.tab1,
+                self.tab3,
+                self.tab4,
+                self.tab5,
+            ]
+        )
+        self.content = ft.Row(
+            expand=True,
+            controls=[
+                self.leftTabChBar,
+                self.rgtTabs,
+            ]
+        )
+
+    def set_init(self):
+        self.leftTabChBar.set_tabBtn_on_click(self._left_tabBtn_event)
+
+    def tab_change(self, to_tab_idx:en.TabIdx):
+        for _tab in self.rgtTabs.controls:
+            if not isinstance(_tab, itf.Rgt_col_TabBase): continue
+            if _tab.tabIdx == to_tab_idx: _tab.visible = True
+            elif _tab.tabIdx == 99: pass
+            elif _tab.tabIdx == 2: pass
+            else: _tab.visible = False
+        self.update()
+    def _left_tabBtn_event(self, e:ft.ControlEvent):
+        if not isinstance(e.control, itf.Left_btn_TabChange): return
+        self.tab_change(e.control.tabIdx)
+        self.update()
 
 
 class App_ExitConfirmDlg(itf.App_ExitConfirmDlg):
--- app/beta6.0/frmInterfaces.py
+++ app/beta6.0/frmInterfaces.py
@@ -523,55 +523,7 @@ class Rgt_tab_5_GJFPreview(Rgt_col_TabBase):
                 case 'OTHER_FUNC2':
                     _btmBtn.visible = False
 
-#*メインフレーム。タブ変更機能のみ持たせた容器。
-class AppMainFrame(ft.Container):
-    def __init__(self):
-        super().__init__(
-            expand=True,
-        )
-        self.leftTabChBar = Left_box_TabChangeBar()
-        self.tab99 = Rgt_tab_99_PlaceHolder()
-        self.tab0 = Rgt_tab_0_CIFSelect()
-        self.tab1 = Rgt_tab_1_CIFPreview()
-        self.tab2 = Rgt_tab_2_AppLogs()
-        self.tab3 = Rgt_tab_3_BuilderResult()
-        self.tab4 = Rgt_tab_4_MISelect()
-        self.tab5 = Rgt_tab_5_GJFPreview()
-        self.rgtTabs = ft.Stack(
-            expand=3,
-            controls=[
-                self.tab99,
-                self.tab2,
-                self.tab0,
-                self.tab1,
-                self.tab3,
-                self.tab4,
-                self.tab5,
-            ]
-        )
-        self.content = ft.Row(
-            expand=True,
-            controls=[
-                self.leftTabChBar,
-                self.rgtTabs,
-            ]
-        )
 
-    def set_init(self):
-        self.leftTabChBar.set_tabBtn_on_click(self._left_tabBtn_event)
-
-    def tab_change(self, to_tab_idx:en.TabIdx):
-        for _tab in self.rgtTabs.controls:
-            if not isinstance(_tab, Rgt_col_TabBase): continue
-            if _tab.tabIdx == to_tab_idx: _tab.visible = True
-            elif _tab.tabIdx == 99: pass
-            elif _tab.tabIdx == 2: pass
-            else: _tab.visible = False
-        self.update()
-    def _left_tabBtn_event(self, e:ft.ControlEvent):
-        if not isinstance(e.control, Left_btn_TabChange): return
-        self.tab_change(e.control.tabIdx)
-        self.update()
 
 #?アプリ終了時の確認ダイアログ。終了時動作はダミー。実装時に上書き必須。
 class App_ExitConfirmDlg(ft.AlertDialog):
@@ -590,3 +542,42 @@ class App_ExitConfirmDlg(ft.AlertDialog):
     def yes_clicked(self, e):
         raise NameError("このメソッドは実装時に上書きが必須です。")
 
+
+class App_list_CellData_Value():
+    def __init__(self, cell_data_label:en.CellDataLabel, value:str):
+        self.LABEL = cell_data_label
+        self.VALUE = value
+
+class App_list_CellData(List[App_list_CellData_Value]):
+    def __init__(self):
+        pass
+
+    def append(self, object: App_list_CellData_Value):
+        for _data in self:
+            if object.LABEL == _data.LABEL:
+                if object.LABEL == en.CellDataLabel.ATOMS:
+                    if object.VALUE == _data.VALUE:
+                        self.remove(_data)
+                        break
+                else:
+                    self.remove(_data)
+                    break
+        super().append(object)
+
+    def append_value(self, cell_data_label:en.CellDataLabel, value:str):
+        self.append(App_list_CellData_Value(cell_data_label, value))
+
+    def get_value(self, cell_data_label:en.CellDataLabel) -> App_list_CellData_Value:
+        for _value in self:
+            if _value.LABEL == cell_data_label:
+                return _value
+        raise IndexError(f'{cell_data_label} is not in this list.')
+
+if __name__ == '__main__':
+    test_filedata = App_list_CellData()
+    test_filedata.append_value(en.CellDataLabel.STATE, "test")
+    for _data in test_filedata:
+        print(f'{_data.LABEL.value}:{_data.VALUE}')
+    test_filedata.append_value(en.CellDataLabel.STATE, "teeeest")
+    for _data in test_filedata:
+        print(f'{_data.LABEL.value}:{_data.VALUE}')
